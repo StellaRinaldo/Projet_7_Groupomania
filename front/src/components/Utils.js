da49f21a95d39d91508d212c0bba2c34163630f8
Stella Rinaldo <stella.rinaldo95@gmail.com>
2022-08-22 17:25:22 +0200

    Optimisation du code

--- front/src/components/Utils.js
+++ front/src/components/Utils.js
@@ -2,8 +2,6 @@ export const dateParser = (num) => {
     let options = {
       hour: "2-digit",
       minute: "2-digit",
-      //second: "2-digit",
-      //weekday: "long",
       year: "numeric",
       month: "short",
       day: "numeric",
@@ -20,8 +18,6 @@ export const dateParser = (num) => {
     let options = {
       hour: "2-digit",
       minute: "2-digit",
-      //second: "2-digit",
-      //weekday: "long",
       year: "numeric",
       month: "short",
       day: "numeric",
--- front/src/reducers/index.js
+++ front/src/reducers/index.js
@@ -3,12 +3,10 @@ import userReducer from './user.reducer';
 import usersReducer from './users.reducer';
 import postReducer from './post.reducer';
 import errorReducer from './error.reducer';
-//import allPostsReducer from './allPosts.reducer';
 
 export default combineReducers({
   userReducer,
   usersReducer,
   postReducer,
   errorReducer
-  //allPostsReducer,
 });
